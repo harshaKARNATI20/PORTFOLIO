ec870e6980520b5c09e23fad1b3e1965868dfcf5
Harsha Vardhan <harshavardhanhasty@gmail.com>
2025-07-08 21:39:08 +0530

    Updated Projects

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -38,3 +38,55 @@ export function Frameworks() {
 const Icon = ({ src }) => (
   <img src={src} className="duration-200 rounded-sm hover:scale-110" />
 );
+// import { OrbitingCircles } from "./OrbitingCircles";
+
+// export function Frameworks() {
+//   const skills = [
+//     "react",
+//     "redux",
+//     "nodejs",
+//     "express",
+//     "mongodb",
+//     "laravel",
+//     "php",
+//     "mysql",
+//     "blade",
+//     "unity",
+//     "csharp",
+//     "blender",
+//     "html5",
+//     "css3",
+//     "tailwindcss",
+//     "javascript",
+//     "vitejs",
+//     "git",
+//     "figma",
+//     "excel",
+//     "jwt",
+//     "chartjs",
+//   ];
+
+//   const fileFormats = ["svg", "png", "jpg"];
+//   const logoPaths = skills.flatMap((skill) =>
+//     fileFormats.map((ext) => `assets/logos/${skill}.${ext}`)
+//   );
+
+//   return (
+//     <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
+//       <OrbitingCircles iconSize={40}>
+//         {logoPaths.map((src, index) => (
+//           <Icon key={index} src={src} />
+//         ))}
+//       </OrbitingCircles>
+//       <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
+//         {[...logoPaths].reverse().map((src, index) => (
+//           <Icon key={index} src={src} />
+//         ))}
+//       </OrbitingCircles>
+//     </div>
+//   );
+// }
+
+// const Icon = ({ src }) => (
+//   <img src={src} className="duration-200 rounded-sm hover:scale-110" />
+// );
